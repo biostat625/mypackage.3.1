f3eee5d88a4e1f1661890d3ffffd19f367aa2957
Hui Jiang <jianghui@umich.edu>
2024-11-05 13:30:38 -0500

    added document for timesTwo

--- src/testcpp.cpp
+++ src/testcpp.cpp
@@ -1,6 +1,17 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//'timesTwo
+//'
+//'Multiply a number by two
+//'
+//'@param x input value
+//'
+//'@return x times two
+//'
+//'@examples
+//'timesTwo(3)
+//'
 //' @export
 // [[Rcpp::export]]
 NumericVector timesTwo(NumericVector x) {
